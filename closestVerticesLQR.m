9a5d4d7f11dd6987a198cd568412d99db2442d3d
Christian Haigh <chaigh@college.harvard.edu>
2016-04-14 00:12:32 -0400

    added Floyd-Marshall, started on PRM

---
+++ closestVerticesLQR.m
@@ -0,0 +1,64 @@
+function [closest_vertices, K, ix] = closestVerticesLQR(tree_verts,xy,k_verts)
+	g = 9.81; 
+	b = 0.1;
+	distances = repmat(inf,1,k_verts);
+	closest_vertices = repmat(tree_verts(1:2,1),1,k_verts);
+	lower_bound = -pi/2;
+	upper_bound = 3*pi/2;
+	while xy(1) > 3*pi/2
+		xy(1) = xy(1) - 2*pi; 
+	end  
+	while xy(1) < -pi/2 
+		xy(1) = xy(1) + 2*pi;
+	end 
+	R = 0.1; 
+	Q = eye(2);
+	A = [0,1;-g*cos(xy(1)),-b];
+	B = [0;1];
+	[K,S] = lqr(A,B,Q,R);
+	tree_length = size(tree_verts);
+	ix = 1;
+	for k = 1:tree_length(2)
+		new_vert = tree_verts(1:2,k);
+		if new_vert(1) > xy(1)
+			wrap = 2*pi;
+		else 
+			wrap = -2*pi;
+		end 
+		new_vert_a = new_vert; 
+		new_vert_b = [new_vert(1)-wrap;new_vert(2)];
+		if (new_vert_a-xy)'*S*(new_vert_a-xy) < (new_vert_b-xy)'*S*(new_vert_b-xy)
+			dist = (new_vert_a-xy)'*S*(new_vert_a-xy);
+			new_vert = new_vert_a;
+		else 
+			dist = (new_vert_b-xy)'*S*(new_vert_b-xy);
+			new_vert = new_vert_b;
+		end 
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		% Change the vertices if the new distances are smaller 
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		dist_diff = 0
+		for l = 1:k_verts
+			dist_check = distances(l) - dist
+			if dist_check > dist_diff
+				dist_diff = dist_check
+				vert_change = l
+			end 
+		end 
+		if dist_diff > 0
+			distances(vert_change) = distances(vert_change) - dist_diff
+			closest_vertices(vert_change) = new_vert
+		end 
+	end 
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% make sure that all the closest vertices are within the frame
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	for l = 1:k_verts
+		while(closest_vertices(1,l) > 3*pi/2)
+			closest_vertices(1,l) = closest_vertices(1,l) - 2*pi
+		end
+		while(closest_vertices(1,l) < -pi/2)
+			closest_vertices(1,l) = closest_vertices(1,l) + 2*pi
+		end
+	end
+end 
---
+++ prm.m
@@ -0,0 +1,141 @@
+%% Code setup (Do not modify, but please read) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Bounds on world
+world_bounds_th = [-pi/2,(3/2)*pi];
+world_bounds_thdot = [-10,10];
+
+xs = (world_bounds_th(2) - world_bounds_th(1))*rand(1) + world_bounds_th(1);
+ys = (world_bounds_thdot(2) - world_bounds_thdot(1))*rand(1) + world_bounds_thdot(1);
+
+
+% Start and goal positions
+figure(1); clf;
+xy_start = [xs;ys]; plot(xy_start(1),xy_start(2),'bo','MarkerFaceColor','b','MarkerSize',10);
+xy_goal = [pi;0]; plot(xy_goal(1),xy_goal(2),'go','MarkerFaceColor','g','MarkerSize',10); drawnow;
+
+
+% Initialize PRM. The PRM will be represented as a 2 x N list of points, and the connections
+% between vertices will be represented as a N x N matrix of edges. Finally, the distances 
+% between vertices will be represented as a N x N matrix of distances. 
+
+prm_verts = zeros(2,100);
+prm_verts(:,1) = xy_start;
+
+N = 1;
+nearGoal = false; % This will be set to true if goal has been reached
+minDistGoal = 0.25; 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The number of closest vertices considered for each new point
+k_verts = 5
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+method = 'lqr'; % LQR distance metric 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+figure(1); hold on;
+axis([world_bounds_th, world_bounds_thdot]);
+hxy = plot(0,0,'ro');
+% PRM algorithm
+while N < 10;
+    % Sample point
+    rnd = rand(1);
+    % With probability 0.05, sample a random point from another tree. 
+    if rnd < 0.05
+        xy = xy_goal;
+    else
+        % Sample from space with probability 0.95
+        xs = (world_bounds_th(2) - world_bounds_th(1))*rand(1) + world_bounds_th(1);
+        ys = (world_bounds_thdot(2) - world_bounds_thdot(1))*rand(1) + world_bounds_thdot(1);
+        xy = [xs;ys];
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Extend closest vertex in direction of random point
+    if strcmp(method, 'lqr')
+        [closest_vert,K,ix] = closestVerticesLQR(prm_verts(:,1:N),xy);
+
+        new_vert = extendLQR(closest_vert,xy,K); 
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Extend closest k - 1 vertices to the newly formed vertex if they can get there
+    if strcmp(method, 'lqr')
+        [closest_vertices,K,ix] = closestVerticesLQR(prm_verts(:,1:N),new_vert,k_verts);
+        % Need to reprogram this function
+        new_vert = extendLQR(closest_vert,xy,K); 
+        % Need to also include all connections from new node to all other nodes
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+    delete(hxy);
+    figure(1);
+    hxy = plot(xy(1),xy(2),'r.');axis([world_bounds_th, world_bounds_thdot]); 
+
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+     
+    % Plot extension (Comment the next few lines out if you want your code to
+    % run a bit quicker. The plotting is useful for debugging though.)
+    figure(1)
+    hold on
+    plot(new_vert(1),new_vert(2),'bo','MarkerFaceColor','b','MarkerSize',5);
+    % Plot line (but only if we are not wrapping to the other side of the
+    % plot)
+    if abs(closest_vert(1) - new_vert(1)) < 0.75*(2*pi)
+        line([closest_vert(1),new_vert(1)],[closest_vert(2),new_vert(2)]);
+    end
+    axis([world_bounds_th, world_bounds_thdot]);
+
+    
+    %% DO NOT MODIFY THIS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % If it is collision free, add it to tree    
+    N = N+1;
+    if N > size(rrt_verts,2)
+        rrt_verts = [rrt_verts zeros(size(rrt_verts))];
+    end
+    rrt_verts(:,N) = [new_vert;ix];
+    
+    % Check if we have reached goal
+    if norm(xy_goal-new_vert) < minDistGoal
+        break;
+    end
+    disp(N);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+       
+end
+
+% Plot vertices in RRT
+hold on;
+plot(rrt_verts(1,:),rrt_verts(2,:),'bo','MarkerFaceColor','b','MarkerSize',5);
+xys = []
+while(N>0)
+    N = rrt_verts(3,N);
+    if N > 0
+        disp(N);
+        xys = [rrt_verts(1:2,N),xys];
+    end 
+end 
+disp(xys);
+ts = [];
+for i=1:size(xys,2)
+    ts = [ts, 0.1*(i-1)];
+end 
+xy_length = size(xys)
+for k = 1:xy_length(2)-1
+    first_vert = xys(:,k);
+    second_vert = xys(:,k+1);
+    disp(second_vert);
+    if abs(first_vert(1) - second_vert(1)) < 0.75*(2*pi)
+        line([first_vert(1),second_vert(1)],[first_vert(2),second_vert(2)],'Color','r');
+    end
+end
+%disp(ts);
+%pd = PendulumPlant;
+%pv = PendulumVisualizer();
+%traj = PPTrajectory(foh(ts,xys));
+%traj = traj.setOutputFrame(pv.getInputFrame());
+%playback(pv,traj);
